dbd69edf7f932a2672f9da8a6d24fcf5d40f3b4f
chuh007 <chhuh007@gmail.com>
2025-04-08 16:42:04 +0900

    feat : TestDrag

--- 0304_2Class_1/Assets/Blade/Test/RoadGridSystem.cs
+++ 0304_2Class_1/Assets/Blade/Test/RoadGridSystem.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Blade.Players;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 namespace Blade.Test
 {
@@ -13,7 +16,6 @@ namespace Blade.Test
         [SerializeField] private PlayerInputSO playerInput;
         [SerializeField] private Grid mapGrid;
         [SerializeField] private GameObject roadBlockPrefab;
-
         public UnityEvent<bool> ConstructionModeChange;
         public UnityEvent UpdateNavigation;
         
@@ -21,6 +23,9 @@ namespace Blade.Test
 
         private HashSet<Vector3Int> _roadPoints;
 
+        [SerializeField] private bool canCombineMesh;
+        private MeshFilter _parentMeshFilter;
+        
         public bool ConstructionMode
         {
             get => _constructionMode;
@@ -33,6 +38,9 @@ namespace Blade.Test
 
         private void Awake()
         {
+            _parentMeshFilter = GetComponent<MeshFilter>();
+            _parentMeshFilter.mesh = new Mesh(); // 빈 메시 하나를 추가한다.
+            
             _roadPoints = new HashSet<Vector3Int>();
             playerInput.OnAttackPressed += HandleClick;
         }
@@ -54,7 +62,38 @@ namespace Blade.Test
                 Vector3 center = mapGrid.GetCellCenterWorld(cellPoint);
                 GameObject road = Instantiate(roadBlockPrefab, center, Quaternion.identity);
                 road.transform.SetParent(transform);
+
+                if (canCombineMesh)
+                {
+                    CombineMesh();
+                }
+                
+                UpdateNavigation?.Invoke();
             }
+        }
+
+        private void CombineMesh()
+        {
+            MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>(false);
+            CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+            int vertexCnt = 0;
+            for (int i = 0; i < meshFilters.Length; i++)
+            {
+                if(meshFilters[i].sharedMesh == null) continue;
+                
+                combine[i].mesh = meshFilters[i].sharedMesh;
+                combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+
+                vertexCnt += meshFilters[i].sharedMesh.vertexCount;
+            }
+            
+            _parentMeshFilter.mesh = new Mesh(); // 새로운 메시를 만들어주고
+            if (vertexCnt > 65535)
+                _parentMeshFilter.mesh.indexFormat = IndexFormat.UInt32;
+            
+            _parentMeshFilter.mesh.CombineMeshes(combine);
+            gameObject.SetActive(true);
         }
 
         private void Update()
@@ -62,5 +101,15 @@ namespace Blade.Test
             if(Keyboard.current.tabKey.wasPressedThisFrame)
                 ConstructionMode = !ConstructionMode;
         }
+
+        [SerializeField] private GameObject agent;
+        
+        public void DeployAgent()
+        {
+            Vector3Int cellPoint = _roadPoints.Skip(Random.Range(0,_roadPoints.Count)).First();
+            Vector3 position = mapGrid.GetCellCenterWorld(cellPoint);
+            Instantiate(agent, position, Quaternion.identity);
+        }
+        
     }
 }
