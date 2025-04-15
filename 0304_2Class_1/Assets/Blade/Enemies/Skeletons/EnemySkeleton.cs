729deb218feaeb83ad7df8ae0d3e44ed202124c9
chuh007 <chhuh007@gmail.com>
2025-04-15 16:41:00 +0900

    feat : BT

--- 0304_2Class_1/Assets/Blade/Enemies/Skeletons/EnemySkeleton.cs
+++ 0304_2Class_1/Assets/Blade/Enemies/Skeletons/EnemySkeleton.cs
@@ -1,9 +1,33 @@
+using System;
 using UnityEngine;
 
 namespace Blade.Enemies.Skeletons
 {
     public class EnemySkeleton : Enemy
     {
-        
+        [SerializeField] private Vector3[] positions;
+
+        private int _currentIndex = 0;
+        private NavMovement _navMovement;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _navMovement = GetCompo<NavMovement>();
+        }
+
+        private void Start()
+        {
+            _navMovement.SetDestination(positions[_currentIndex]);
+        }
+
+        private void Update()
+        {
+            if (_navMovement.IsArrived)
+            {
+                _currentIndex = (_currentIndex + 1) % positions.Length;
+                _navMovement.SetDestination(positions[_currentIndex]);
+            }
+        }
     }
 }
